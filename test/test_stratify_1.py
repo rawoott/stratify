d277e515d1044c9b96b30fbe0ac9bd8104ca58a9
rawoott <37331566+rawoott@users.noreply.github.com>
2018-05-21 22:20:58 +0200

    Update test_stratify_1.py
    fixed ithink

--- test/test_stratify_1.py
+++ test/test_stratify_1.py
@@ -15,7 +15,7 @@ class TestExclude(unittest.TestCase):
     def test_centre2(self):
         evecs=[[0,0,0],[-1,0,1],[1,0,1],[0,4,0]]
         c1 = stratify.getCentroid(evecs,0,[0,1])
-        self.assertEqual(c1,1)
+        self.assertEqual(c1,-0.5)
 
 if __name__ == '__main__':
 
